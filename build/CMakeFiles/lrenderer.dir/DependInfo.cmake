84e67a69ea4476d4f29591f522ed038bb37bc702
Haige <hg@ubuntu>
2022-11-09 09:35:56 +0800

    add files

---
+++ build/CMakeFiles/lrenderer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hg/LQ/lrenderer/src/main.cpp" "/home/hg/LQ/lrenderer/build/CMakeFiles/lrenderer.dir/src/main.cpp.o"
+  "/home/hg/LQ/lrenderer/src/tgaimage.cpp" "/home/hg/LQ/lrenderer/build/CMakeFiles/lrenderer.dir/src/tgaimage.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../obj"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lrenderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../obj/lrenderer"
+  "../obj/lrenderer.pdb"
+  "CMakeFiles/lrenderer.dir/src/main.cpp.o"
+  "CMakeFiles/lrenderer.dir/src/tgaimage.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lrenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
